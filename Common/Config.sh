40fe4a13d3879573aa319e3f57b5d7df629d7417
otsuki154 <nvthanh15490@gmail.com>
2020-08-30 15:41:45 +0900

    first commit

---
+++ Common/Config.sh
@@ -0,0 +1,7 @@
+#
+# グローバル変数定義クラス
+
+#ログファイル
+export LOG_FILE="$(pwd)/Log/$(date +%Y%m%d)_Execute.log"
+#本日
+export NOW=$(date +%F)
---
+++ CopyImage.sh
@@ -0,0 +1,10 @@
+#! /bin/bash -vex
+#
+# コマンドテストため、作成
+
+#共通クラスインポート
+source "$(pwd)/Common/Common.sh"
+
+LogOut "実行開始"
+MoveAllFiles /Users/ThanhNV177/Downloads/ /Pictures/20200812_SinhNhatHaru/ JPG
+LogOut "実行終了"
